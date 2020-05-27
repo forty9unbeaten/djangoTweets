e9d21e2f7c3c116c75215b9d6f4de972d85204bc
Rob Spears <rob.sp3ars@gmail.com>
2020-05-27 10:35:25 -0400

    Create custom TwitterUser model

--- twitteruser/models.py
+++ twitteruser/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class TwitterUser(AbstractUser):
+    display_name = models.CharField(max_length=50)
+    bio = models.CharField(max_length=140)
+    following = models.ManyToManyField(to='self', symmetrical=False)
