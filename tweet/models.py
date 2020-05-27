4ec139b5648f8112ca1371bdee5f64d031e0b462
Rob Spears <rob.sp3ars@gmail.com>
2020-05-27 11:03:22 -0400

    Create Tweet model

--- tweet/models.py
+++ tweet/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from twitteruser.models import TwitterUser
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Tweet(models.Model):
+    content = models.CharField(max_length=140)
+    composer = models.ForeignKey(to=TwitterUser, on_delete=models.CASCADE)
+    create_date = models.DateTimeField(default=timezone.now)
