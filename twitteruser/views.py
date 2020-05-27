f59a301678618f367e6f3462c7f12f3ea3483f0d
Rob Spears <rob.sp3ars@gmail.com>
2020-05-27 13:27:46 -0400

    Render registration form and registration template

--- twitteruser/views.py
+++ twitteruser/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from twitteruser.forms import RegisterForm
 
 # Create your views here.
+
+
+def register_view(request):
+    form = RegisterForm()
+    return render(
+        request,
+        'twitteruser/register.html',
+        {
+            'page_title': 'Registration',
+            'form': form
+        })
