4d00adde1649e08b90c91a7c14f27201cf6deeb8
Rob Spears <rob.sp3ars@gmail.com>
2020-05-28 13:10:33 -0400

    add url path for viewing individual tweets

--- tweet/urls.py
+++ tweet/urls.py
@@ -3,5 +3,6 @@ from tweet import views
 
 url_paths = [
     path('', views.homepage_view, name='homepage'),
-    path('newtweet/', views.new_tweet_view, name='new_tweet')
+    path('newtweet/', views.new_tweet_view, name='new_tweet'),
+    path('tweet/<int:tweetId>', views.tweet_view, name='single_tweet')
 ]
