1b35cdadc9cc14c00377f39a0ba1c421bd973575
Rob Spears <rob.sp3ars@gmail.com>
2020-05-28 11:15:52 -0400

    Show content of tweet as title on admin page

--- tweet/models.py
+++ tweet/models.py
@@ -9,3 +9,6 @@ class Tweet(models.Model):
     content = models.CharField(max_length=140)
     composer = models.ForeignKey(to=TwitterUser, on_delete=models.CASCADE)
     create_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.content
