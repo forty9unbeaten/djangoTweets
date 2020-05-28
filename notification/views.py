15cbe50fb689e1706feef1320f764d2b1fc174bb
Rob Spears <rob.sp3ars@gmail.com>
2020-05-28 15:41:20 -0400

    Add view that renders notification page template

--- notification/views.py
+++ notification/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from tweet.utils import get_new_notifications
 
 # Create your views here.
+
+
+def notification_view(request):
+    new_notifications = get_new_notifications(request.user)
+    new_notifications = new_notifications.order_by('-create_date')
+    if new_notifications:
+        tweets = []
+        for notification in new_notifications:
+            tweets.append(notification.tweet)
+            notification.delete()
+    else:
+        tweets = None
+
+    return render(
+        request,
+        'notification/notifications.html',
+        {
+            'tweets': tweets
+        }
+    )
