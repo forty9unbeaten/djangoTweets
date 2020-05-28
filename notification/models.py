e0a4e76e10ed2451216839a775a90e60c026cd88
Rob Spears <rob.sp3ars@gmail.com>
2020-05-28 13:36:33 -0400

    Add notification model

--- notification/models.py
+++ notification/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from twitteruser.models import TwitterUser
+from tweet.models import Tweet
 
 # Create your models here.
+
+
+class Notification(models.Model):
+    intended_for: models.ForeignKey(to=TwitterUser, on_delete=models.CASCADE)
+    tweet: models.ForeignKey(to=Tweet, on_delete=models.CASCADE)
