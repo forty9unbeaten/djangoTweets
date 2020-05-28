5978cd314943f7b367dbc72476a49c2bde071f15
Rob Spears <rob.sp3ars@gmail.com>
2020-05-28 11:17:01 -0400

    Add url path for sending new tweet

--- tweet/urls.py
+++ tweet/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from tweet import views
 
 url_paths = [
-    path('', views.homepage_view, name='homepage')
+    path('', views.homepage_view, name='homepage'),
+    path('newtweet/', views.new_tweet_view, name='new_tweet')
 ]
