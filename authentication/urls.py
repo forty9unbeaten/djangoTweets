eb955fc22fe51c23ab75fa314958734e84127a5f
Rob Spears <rob.sp3ars@gmail.com>
2020-05-27 22:31:40 -0400

    Add logout url path

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from authentication import views
 
 url_paths = [
-    path('login/', views.login_view, name='login')
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout')
 ]
