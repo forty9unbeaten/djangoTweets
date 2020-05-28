e78642a4240162ec20752af590c5a11fdaf9ef3a
Rob Spears <rob.sp3ars@gmail.com>
2020-05-28 15:40:43 -0400

    Final Notification model implementation

--- notification/models.py
+++ notification/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from twitteruser.models import TwitterUser
 from tweet.models import Tweet
 
@@ -6,5 +7,10 @@ from tweet.models import Tweet
 
 
 class Notification(models.Model):
-    intended_for: models.ForeignKey(to=TwitterUser, on_delete=models.CASCADE)
-    tweet: models.ForeignKey(to=Tweet, on_delete=models.CASCADE)
+    intended_for = models.ForeignKey(
+        to=TwitterUser, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(to=Tweet, on_delete=models.CASCADE)
+    create_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.intended_for.username
