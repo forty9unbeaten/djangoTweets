5b6f195431c171462d81664c5d9bbaf1863f54c4
Rob Spears <rob.sp3ars@gmail.com>
2020-05-27 22:37:20 -0400

    Add user detail url path

--- twitteruser/urls.py
+++ twitteruser/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from twitteruser import views
 
 url_paths = [
-    path('register/', views.register_view, name='register')
+    path('register/', views.register_view, name='register'),
+    path('users/<str:username>/', views.user_detail_view, name='user_detail')
 ]
