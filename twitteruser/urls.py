067253d40a302c7a87d141e3380e9cd38dfbac6c
Rob Spears <rob.sp3ars@gmail.com>
2020-05-28 13:11:30 -0400

    Add url oaths for following and unfollowing other users

--- twitteruser/urls.py
+++ twitteruser/urls.py
@@ -3,5 +3,7 @@ from twitteruser import views
 
 url_paths = [
     path('register/', views.register_view, name='register'),
-    path('users/<str:username>/', views.user_detail_view, name='user_detail')
+    path('users/<str:username>/', views.user_detail_view, name='user_detail'),
+    path('users/<str:username>/unfollow', views.unfollow_view),
+    path('users/<str:username>/follow', views.follow_view)
 ]
