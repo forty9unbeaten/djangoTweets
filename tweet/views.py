57be8fc40b249a40b6bb8a1a1375bd09ca8ce0ab
Rob Spears <rob.sp3ars@gmail.com>
2020-05-28 15:46:31 -0400

    determine if new notifications exist before rendering templates for homepage and tweet detail views

--- tweet/views.py
+++ tweet/views.py
@@ -3,7 +3,10 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from tweet.forms import NewTweetForm
 from tweet.models import Tweet
+from tweet.utils import find_mention_notifications, get_new_notifications
 from twitteruser.models import TwitterUser
+from notification.models import Notification
+
 
 # Create your views here.
 
@@ -20,13 +23,20 @@ def homepage_view(request):
     # filter tweets based on ids of people user is following
     tweets = Tweet.objects.filter(
         composer__id__in=following).order_by('-create_date')
+
+    if get_new_notifications(request.user):
+        new_notifications = True
+    else:
+        new_notifications = False
+
     return render(
         request,
         'tweet/homepage.html',
         {
             'user': request.user,
             'form': form,
-            'tweets': tweets
+            'tweets': tweets,
+            'new_notifications': new_notifications
         })
 
 
@@ -41,6 +51,7 @@ def new_tweet_view(request):
                 composer=request.user
             )
             new_tweet.save()
+            find_mention_notifications(new_tweet)
             return HttpResponseRedirect(
                 reverse('homepage')
             )
@@ -48,12 +59,17 @@ def new_tweet_view(request):
 
 def tweet_view(request, tweetId):
     tweet = Tweet.objects.get(id=tweetId)
+    if request.user.is_authenticated and get_new_notifications(request.user):
+        new_notifications = True
+    else:
+        new_notifications = False
 
     return render(
         request,
         'tweet/tweet_detail.html',
         {
             'tweet': tweet,
+            'new_notifications': new_notifications,
             'is_auth': request.user.is_authenticated
         }
     )
