c2dcb17f5238fc1ce496a50f6c5cb95dbd196aa7
Rob Spears <rob.sp3ars@gmail.com>
2020-05-28 11:17:48 -0400

    retrieve tweets from people following on homepage and create view that saves new tweets

--- tweet/views.py
+++ tweet/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from tweet.forms import NewTweetForm
+from tweet.models import Tweet
+from twitteruser.models import TwitterUser
 
 # Create your views here.
 
@@ -8,10 +11,36 @@ from tweet.forms import NewTweetForm
 @login_required
 def homepage_view(request):
     form = NewTweetForm()
+
+    # parse ids from list of people user is following
+    follow_set = request.user.following.get_queryset()
+    following = []
+    for user in follow_set:
+        following.append(user.id)
+    # filter tweets based on ids of people user is following
+    tweets = Tweet.objects.filter(
+        composer__id__in=following).order_by('-create_date')
     return render(
         request,
         'tweet/homepage.html',
         {
             'user': request.user,
-            'form': form
+            'form': form,
+            'tweets': tweets
         })
+
+
+@login_required
+def new_tweet_view(request):
+    if request.method == 'POST':
+        form = NewTweetForm(request.POST)
+        if form.is_valid():
+            content = form.cleaned_data['content']
+            new_tweet = Tweet(
+                content=content,
+                composer=request.user
+            )
+            new_tweet.save()
+            return HttpResponseRedirect(
+                reverse('homepage')
+            )
