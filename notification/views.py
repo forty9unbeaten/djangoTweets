eb2d002108c5695fe941753074194609dab9594d
Rob Spears <rob.sp3ars@gmail.com>
2020-06-03 21:29:20 -0400

    Convert notification view to class-based view

--- notification/views.py
+++ notification/views.py
@@ -1,24 +1,30 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 from tweet.utils import get_new_notifications
 
 # Create your views here.
 
 
-def notification_view(request):
-    new_notifications = get_new_notifications(request.user)
-    new_notifications = new_notifications.order_by('-create_date')
-    if new_notifications:
-        tweets = []
-        for notification in new_notifications:
-            tweets.append(notification.tweet)
-            notification.delete()
-    else:
-        tweets = None
+class Notifications(LoginRequiredMixin, View):
 
-    return render(
-        request,
-        'notification/notifications.html',
-        {
-            'tweets': tweets
-        }
-    )
+    html_template = 'notification/notifications.html'
+
+    def get(self, request):
+        new_notifications = get_new_notifications(request.user)
+        new_notifications = new_notifications.order_by('-create_date')
+        if new_notifications:
+            tweets = []
+            for notification in new_notifications:
+                tweets.append(notification.tweet)
+                notification.delete()
+        else:
+            tweets = None
+
+        return render(
+            request,
+            self.html_template,
+            {
+                'tweets': tweets
+            }
+        )
