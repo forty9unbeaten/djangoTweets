e3fecef5da0f671507086fc5df4f46822863493e
Rob Spears <rob.sp3ars@gmail.com>
2020-06-03 21:14:36 -0400

    Implement user registration class-based view in url paths

--- twitteruser/urls.py
+++ twitteruser/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from twitteruser import views
 
 url_paths = [
-    path('register/', views.register_view, name='register'),
+    path('register/', views.RegisterUser.as_view(), name='register'),
     path('users/<str:username>/', views.user_detail_view, name='user_detail'),
     path('users/<str:username>/unfollow', views.unfollow_view),
     path('users/<str:username>/follow', views.follow_view)
