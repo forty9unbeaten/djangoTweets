0c09cb5a8746b3397d405f9b030cd97f93537557
Rob Spears <rob.sp3ars@gmail.com>
2020-06-03 21:24:28 -0400

    Implement user detail class-based view in url paths

--- twitteruser/urls.py
+++ twitteruser/urls.py
@@ -3,7 +3,7 @@ from twitteruser import views
 
 url_paths = [
     path('register/', views.RegisterUser.as_view(), name='register'),
-    path('users/<str:username>/', views.user_detail_view, name='user_detail'),
+    path('users/<str:username>/', views.UserDetail.as_view(), name='user_detail'),
     path('users/<str:username>/unfollow', views.unfollow_view),
     path('users/<str:username>/follow', views.follow_view)
 ]
