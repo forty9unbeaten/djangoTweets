8f5e5c2329408b13af4cfc5a8fa986e564dad403
Rob Spears <rob.sp3ars@gmail.com>
2020-06-03 21:04:27 -0400

    Implement class-based homepage view in url paths

--- tweet/urls.py
+++ tweet/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from tweet import views
 
 url_paths = [
-    path('', views.homepage_view, name='homepage'),
+    path('', views.HomepageView.as_view(), name='homepage'),
     path('newtweet/', views.new_tweet_view, name='new_tweet'),
     path('tweet/<int:tweetId>', views.tweet_view, name='single_tweet')
 ]
